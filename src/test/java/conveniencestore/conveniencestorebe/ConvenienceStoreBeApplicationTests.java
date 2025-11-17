6e4d31e89f1bfd6eee13996415f450adaefe90f6
JMJ <ghtntkdnsk@naver.com>
2025-11-17 18:08:59 +0900

    first commit

---
+++ src/test/java/conveniencestore/conveniencestorebe/ConvenienceStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package conveniencestore.conveniencestorebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenienceStoreBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
